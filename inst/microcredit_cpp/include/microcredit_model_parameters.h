9b3d261a2a80dfe382ddb7d45e223067b2ffaee3
Ryan Giordano <rgiordan@gmail.com>
2016-06-19 16:32:12 +0200

    Use simpler types

--- inst/microcredit_cpp/include/microcredit_model_parameters.h
+++ inst/microcredit_cpp/include/microcredit_model_parameters.h
@@ -31,40 +31,48 @@ public:
   int n_g;    // The number of groups
   int k;      // The dimension of the means
 
-  VectorParameter<T> e_mu;    // The vector of E(mu)
-  PosDefMatrixParameter<T> e_mu2;   // E(mu mu^T)
+
+  MultivatiateNormal<T> mu;
+
+  // VectorParameter<T> e_mu;    // The vector of E(mu)
+  // PosDefMatrixParameter<T> e_mu2;   // E(mu mu^T)
 
   // Unlike the other parameters, use the natural parameters for lambda.
-  PosDefMatrixParameter<T> lambda_v_par;   // lambda ~ Wishart(v_par, n_par)
-  ScalarParameter<T> lambda_n_par;
+  Wishart<T> lambda;
+  // PosDefMatrixParameter<T> lambda_v_par;   // lambda ~ Wishart(v_par, n_par)
+  // ScalarParameter<T> lambda_n_par;
 
   // A vector of per-group, E(tau), the observation noise precision
-  vector<ScalarParameter<T>> e_tau_vec;
-  vector<ScalarParameter<T>> e_log_tau_vec;  // E(log(tau))
+  vector<Gamma<T>> tau_vec;
+  // vector<ScalarParameter<T>> e_tau_vec;
+  // vector<ScalarParameter<T>> e_log_tau_vec;  // E(log(tau))
 
   // Vectors of the per-group means.
-  vector<VectorParameter<T>> e_mu_g_vec;  // E(mu_k)
-  vector<PosDefMatrixParameter<T>> e_mu2_g_vec; // E(mu_k mu_k^T)
+  vector<MultivatiateNormal<T>> mu_g_vec;
+  // vector<VectorParameter<T>> e_mu_g_vec;  // E(mu_k)
+  // vector<PosDefMatrixParameter<T>> e_mu2_g_vec; // E(mu_k mu_k^T)
 
   // Methods:
   VariationalParameters(int k, int n_g): k(k), n_g(n_g) {
-    e_mu = VectorParameter<T>(k, "e_mu");
-    e_mu2 = PosDefMatrixParameter<T>(k, "e_mu2");
+    mu = MultivatiateNormal<T>(k);
+    // e_mu = VectorParameter<T>(k, "e_mu");
+    // e_mu2 = PosDefMatrixParameter<T>(k, "e_mu2");
 
-    lambda_v_par = PosDefMatrixParameter<T>(k, "lambda_v_par");
-    lambda_n_par = ScalarParameter<T>("lambda_n_par");
+    // lambda_v_par = PosDefMatrixParameter<T>(k, "lambda_v_par");
+    // lambda_n_par = ScalarParameter<T>("lambda_n_par");
+    lambda = Wishart<T>(k);
 
     // Do the mu_g vectors
-    e_mu_g_vec.resize(n_g);
-    e_mu2_g_vec.resize(n_g);
-    e_tau_vec.resize(n_g);
-    e_log_tau_vec.resize(n_g);
+    // e_mu_g_vec.resize(n_g);
+    // e_mu2_g_vec.resize(n_g);
+    // e_tau_vec.resize(n_g);
+    // e_log_tau_vec.resize(n_g);
+    mu_g_vec.resize(n_g);
+    tau_vec.resize(n_g);
 
     for (int g = 0; g < n_g; g++) {
-      e_mu_g_vec[g] = VectorParameter<T>(k, "e_mu_g" + g);
-      e_mu2_g_vec[g] = PosDefMatrixParameter<T>(k, "e_mu2_g" + g);
-      e_tau_vec[g] = ScalarParameter<T>("e_tau_g" + g);
-      e_log_tau_vec[g] = ScalarParameter<T>("e_log_tau_g" + g);
+      mu_g_vec[g] = MultivatiateNormal<T>(k);
+      tau_vec[g] = Gamma<T>();
     }
   };
 
@@ -79,18 +87,27 @@ public:
   operator VariationalParameters<Tnew>() const {
     VariationalParameters<Tnew> vp = VariationalParameters<Tnew>(k, n_g);
 
-    vp.e_mu = e_mu;
-    vp.e_mu2 = e_mu2;
+    // vp.e_mu = e_mu;
+    // vp.e_mu2 = e_mu2;
+    //
+    // vp.lambda_v_par = lambda_v_par;
+    // vp.lambda_n_par = lambda_n_par;
+    //
+    // for (int g = 0; g < n_g; g++) {
+    //   vp.e_mu_g_vec[g] = e_mu_g_vec[g];
+    //   vp.e_mu2_g_vec[g] = e_mu2_g_vec[g];
+    //
+    //   vp.e_tau_vec[g] = e_tau_vec[g];
+    //   vp.e_log_tau_vec[g] = e_log_tau_vec[g];
+    // }
 
-    vp.lambda_v_par = lambda_v_par;
-    vp.lambda_n_par = lambda_n_par;
+    vp.mu = mu;
+
+    vp.lambda = lambda;
 
     for (int g = 0; g < n_g; g++) {
-      vp.e_mu_g_vec[g] = e_mu_g_vec[g];
-      vp.e_mu2_g_vec[g] = e_mu2_g_vec[g];
-
-      vp.e_tau_vec[g] = e_tau_vec[g];
-      vp.e_log_tau_vec[g] = e_log_tau_vec[g];
+      vp.mu_g_vec[g] = mu_g_vec[g];
+      vp.tau_vec[g] = tau_vec[g];
     }
     return vp;
   }
