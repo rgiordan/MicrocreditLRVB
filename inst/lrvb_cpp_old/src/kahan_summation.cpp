f16e84530c98e1f56bbc376eab9b5e1265a3eb8e
Ryan Giordano <rgiordan@gmail.com>
2016-07-14 20:32:28 -0400

    Remove lrvb library copy

--- inst/lrvb_cpp_old/src/kahan_summation.cpp
+++
@@ -1,6 +0,0 @@
-# include "kahan_summation.h"
-
-// Due to a bug in Stan, we can't include their headers here.
-template class KahanAccumulator<double>;
-// template class var KahanSum;
-// template class fvar KahanSum;
--- inst/lrvb_cpp_old/src/variational_parameters.cpp
+++
@@ -1,8 +0,0 @@
-# include "variational_parameters.h"
-
-// The index in a vector of lower diagonal terms of a particular matrix value.
-int get_ud_index(int i, int j) {
-  // If the column is less than the row it's already an upper diagonal index.
-  return j <= i ? (j + i * (i + 1) / 2):
-                  (i + j * (j + 1) / 2);
-};
