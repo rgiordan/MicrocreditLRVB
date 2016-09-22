d5eb3c6b17cd1a4f29573681deda4e1091de84c9
Ryan Giordano <rgiordan@gmail.com>
2016-09-22 11:35:42 -0700

    Integrated epsilon sensitivity working

--- inst/R/microcredit_function_sensitivity.R
+++ inst/R/microcredit_function_sensitivity.R
@@ -7,8 +7,9 @@ library(mvtnorm)
 library(MicrocreditLRVB)
 
 # Load previously computed Stan results
-analysis_name <- "simulated_data_robust"
-#analysis_name <- "simulated_data_nonrobust"
+#analysis_name <- "simulated_data_robust"
+analysis_name <- "simulated_data_nonrobust"
+#analysis_name <- "simulated_data_lambda_beta"
 
 project_directory <-
   file.path(Sys.getenv("GIT_REPO_LOC"), "MicrocreditLRVB/inst/simulated_data")
@@ -76,12 +77,12 @@ vp_opt$mu_loc
 vp_opt_perturb$mu_loc
 
 
+diff_df <- SummarizeRawMomentParameters(GetMomentsFromVector(mp_opt, diff_vec), metric="mean", method="diff")
+ggplot(diff_df) + geom_histogram(aes(x=diff_df$val)) + facet_grid( ~ par)
+
+
 ##############################################
 # Calculate epsilon sensitivity
-
-
-
-#########################################
 # Monte Carlo integrate using a importance sampling
 
 # Monte Carlo samples
@@ -106,7 +107,8 @@ if (FALSE) {
     u_draws[, k] <- grid_range[k] * (2 * runif(n_samples) - 1) + grid_center[k]
   }
 } else {
-  grid_range <- 1.5 * sqrt(diag(lrvb_cov)[vp_indices$mu_loc])
+  # Proposals based on q
+  grid_range <- 2 * sqrt(diag(lrvb_cov)[vp_indices$mu_loc])
   u_cov <- diag(grid_range ^ 2)
   grid_center <- mp_opt$mu_e_vec
   
@@ -125,8 +127,20 @@ GetUniformWeightedInfuenceFunctionVector <- function(mu) {
   u_log_density <- GetULogDensity(mu)
   weight <- exp(mu_t_prior_val- u_log_density)
   sens_pre_factor <- exp(mu_q_res$val - mu_prior_val)
-  sens <- sens_pre_factor * weight * lrvb_pre_factor %*% mu_q_res$grad
-  return(list(sens=sens, weight=weight, sens_pre_factor=sens_pre_factor))
+  prior_ratio <- exp(mu_t_prior_val - mu_prior_val)
+  lrvb_term <- lrvb_pre_factor %*% mu_q_res$grad
+  influence_fun <- sens_pre_factor * lrvb_term 
+  sens <- exp(mu_t_prior_val- u_log_density + mu_q_res$val - mu_prior_val) * lrvb_term
+  
+  # The "mean value theorem" sensitivity
+  mv_term <- (mu_t_prior_val - mu_prior_val) * prior_ratio / (prior_ratio - 1)
+  mv_sens <- lrvb_term * mv_term * exp(mu_q_res$val - u_log_density)
+  mv_int <- (mu_t_prior_val - mu_prior_val) *
+            exp(mu_t_prior_val + mu_prior_val) / (exp(mu_t_prior_val) - exp(mu_prior_val))
+  
+  return(list(sens=sens, weight=weight, sens_pre_factor=sens_pre_factor,
+              influence_fun=influence_fun, prior_ratio=prior_ratio,
+              mv_sens=mv_sens, mv_term=mv_term, mv_int=mv_int))
 }
 
 sens_list <- list()
@@ -137,6 +151,8 @@ for (ind in 1:nrow(u_draws)) {
 }
 close(pb)
 
+
+prior_ratios <- unlist(lapply(sens_list, function(entry) { entry$prior_ratio} ))
 weights <- unlist(lapply(sens_list, function(entry) { entry$weight} ))
 max(weights) / median(weights)
 # hist(log10(weights), 1000)
@@ -145,12 +161,16 @@ mean(weights)
 sum(weights) / n_samples
 
 sens_pre_factors <- unlist(lapply(sens_list, function(entry) { entry$sens_pre_factor} ))
+mv_term <- unlist(lapply(sens_list, function(entry) { entry$mv_term} ))
 max(sens_pre_factors) / median(sens_pre_factors)
 summary(sens_pre_factors)
 
 
 # hist(log10(sens_pre_factors), 1000)
 sens_vec_list <- lapply(sens_list, function(entry) { as.numeric(entry$sens) } )
+mv_sens_vec_list <- lapply(sens_list, function(entry) { as.numeric(entry$mv_sens) } )
+
+influence_vec_list <- lapply(sens_list, function(entry) { as.numeric(entry$influence_fun) } )
 sens_vec_list_squared <- lapply(sens_list, function(entry) { as.numeric(entry$sens) ^ 2 } )
 length(sens_vec_list)
 # sens_vec_mean <- Reduce(`+`, sens_vec_list) / sum(weights)
@@ -158,11 +178,20 @@ sens_vec_mean <- Reduce(`+`, sens_vec_list) / n_samples # TODO: why this and not
 sens_vec_mean_square <- Reduce(`+`, sens_vec_list_squared) / n_samples
 sens_vec_sd <- sqrt(sens_vec_mean_square - sens_vec_mean^2) / sqrt(n_samples)
 
-diagnostic_df <- data.frame(sens_pre_factor=sens_pre_factors, weight=weights)
+mv_sens_vec_mean <- Reduce(`+`, mv_sens_vec_list) / n_samples # TODO: why this and not the sum of the weights?
+mv_sens_vec_list_squared <- lapply(sens_list, function(entry) { as.numeric(entry$mv_sens) ^ 2 } )
+mv_sens_vec_mean_square <- Reduce(`+`, mv_sens_vec_list_squared) / n_samples
+mv_sens_vec_sd <- sqrt(mv_sens_vec_mean_square - mv_sens_vec_mean^2) / sqrt(n_samples)
+
+
+diagnostic_df <- data.frame(sens_pre_factor=sens_pre_factors, weight=weights, prior_ratios=prior_ratios, mv_term=mv_term)
 diagnostic_df <- cbind(diagnostic_df, data.frame(u_draws))
 mean(sens_pre_factors)
 sd(sens_pre_factors)
 # ggplot(diagnostic_df) + geom_point(aes(x=X1, y=X2, color=sens_pre_factor))
+# ggplot(diagnostic_df) + geom_point(aes(x=X1, y=X2, color=log10(prior_ratios))) + scale_color_gradient2()
+ggplot(diagnostic_df) + geom_point(aes(x=X1, y=X2, color=mv_term)) + scale_color_gradient2()
+
 # hist((sens_pre_factors), 100)
 # hist(log10(weights))
 
@@ -173,98 +202,29 @@ sens_results <-
   rbind(
     SummarizeRawMomentParameters(GetMomentsFromVector(mp_opt, sens_vec_mean), metric="mean", method="sens"),
     SummarizeRawMomentParameters(GetMomentsFromVector(mp_opt, sens_vec_sd), metric="sd", method="sens"),
+    SummarizeRawMomentParameters(GetMomentsFromVector(mp_opt, mv_sens_vec_mean), metric="mean", method="mv_sens"),
+    SummarizeRawMomentParameters(GetMomentsFromVector(mp_opt, mv_sens_vec_sd), metric="sd", method="mv_sens"),
     SummarizeRawMomentParameters(GetMomentsFromVector(mp_opt, diff_vec),      metric="mean", method="diff")) %>%
   dcast(par + component + group ~ method + metric, value.var="val")
 
 ggplot(sens_results) +
-  geom_errorbar(aes(x=diff_mean, y=sens_mean, ymax=sens_mean + 2 * sens_sd, ymin = sens_mean - 2 * sens_sd), color="gray") +
+  geom_errorbar(aes(x=diff_mean, y=mv_sens_mean,
+                    ymax=mv_sens_mean + 2 * mv_sens_sd,
+                    ymin=mv_sens_mean - 2 * mv_sens_sd), color="gray") +
+  geom_point(aes(x=diff_mean, y=mv_sens_mean, color=par)) +
+  geom_abline((aes(intercept=0, slope=1)))
+
+ggplot(sens_results) +
+  geom_errorbar(aes(x=diff_mean, y=sens_mean,
+                    ymax=sens_mean + 2 * sens_sd,
+                    ymin=sens_mean - 2 * sens_sd), color="gray") +
   geom_point(aes(x=diff_mean, y=sens_mean, color=par)) +
   geom_abline((aes(intercept=0, slope=1)))
 
 
+
+#######################################
 # Look at one component in detail
-
-component <- mp_indices$mu_e_vec[1]; component_name <- "E_q[mu[1]]"
-# component <- mp_indices$mu_e_vec[2]; component_name <- "E_q[mu[2]]"
-# component <- mp_indices$lambda_e[1, 1]; component_name <- "E_q[lambda[1, 1]]"
-# component <- mp_indices$lambda_e[2, 2]; component_name <- "E_q[lambda[2, 2]]"
-# component <- mp_indices$lambda_e[1, 2]; component_name <- "E_q[lambda[1, 2]]"
-# component <- mp_indices$tau[[1]]$e_log; component_name <- "E_q[log(tau[1])]"
-# component <- mp_indices$mu_g[[7]]$e_vec[1]; component_name <- "E_q[mu_g[7]][1]"
-
-comp_sens_vec <- unlist(lapply(sens_vec_list, function(entry) { as.numeric(entry[component]) } ))
-q_comp_sens_vec <- unlist(lapply(q_sens_vec_list, function(entry) { as.numeric(entry[component]) }))
-
-component_df <- cbind(data.frame(sens_vec=comp_sens_vec), diagnostic_df)
-
-ggplot(component_df) + geom_point(aes(x=X1, y=X2, color=sens_vec)) +
-  scale_color_gradient2()
-
-# ggplot(component_df) + geom_histogram(aes(x=log10(sens_vec)), bins=1000)
-
-mean(component_df$sens_vec)
-sens_vec_sd[component]
-
-#########################################
-# Grid integrate
-
-# component <- mp_indices$mu_e_vec[1]; component_name <- "E_q[mu[1]]"
-# component <- mp_indices$mu_e_vec[2]; component_name <- "E_q[mu[2]]"
-# component <- mp_indices$lambda_e[1, 1]; component_name <- "E_q[lambda[1, 1]]"
-# component <- mp_indices$lambda_e[2, 2]; component_name <- "E_q[lambda[2, 2]]"
-# component <- mp_indices$lambda_e[1, 2]; component_name <- "E_q[lambda[1, 2]]"
-# component <- mp_indices$tau[[1]]$e_log; component_name <- "E_q[log(tau[1])]"
-component <- mp_indices$mu_g[[10]]$e_vec[1]; component_name <- "E_q[mu_g[10]][1]"
-
-lrvb_pre_factor <- -1 * lrvb_terms$jac %*% solve(lrvb_terms$elbo_hess)
-
-GetWeightedInfuenceFunctionVectorGrid <- function(mu) {
-  mu_prior_val <- GetMuLogPrior(mu, pp)
-  mu_q_res <- GetMuLogDensity(mu, vp_opt, pp, TRUE)
-  mu_t_prior_val <- GetMuLogStudentTPrior(mu, pp_perturb)
-  
-  sens_pre_factor <- exp(mu_q_res$val + mu_t_prior_val - mu_prior_val)
-  # cat(mu_q_res$val, " ", mu_t_prior_val, " ", mu_prior_val, "\n")
-  sens_lrvb_term <- lrvb_pre_factor %*% mu_q_res$grad
-  sens_vec <- sens_lrvb_term * sens_pre_factor
-  # return(mu_q_res$val + mu_t_prior_val - mu_prior_val)
-  # return(sens_lrvb_term[component])
-  return(sens_vec[component])
-  # return(exp(mu_q_res$val))
-}
-
-grid_range <- 3 * sqrt(diag(lrvb_cov)[vp_indices$mu_loc])
-
-grid_n <- 50
-mu_influence <- EvaluateOn2dGrid(FUN=GetWeightedInfuenceFunctionVectorGrid,
-                                 mp_opt$mu_e_vec, -grid_range[1], grid_range[1], -grid_range[2], grid_range[2],
-                                 len=grid_n)
-sum(mu_influence$val) * (2 * grid_range[1] / grid_n) * (2 * grid_range[2] / grid_n)
-diff_vec[component]
-sens_vec_mean[component]
-
-# val_clip <- 10
-# mu_influence$val <- ifelse(abs(mu_influence$val) > val_clip, val_clip * sign(mu_influence$val), mu_influence$val)
-ggplot(mu_influence) +
-  geom_tile(aes(x=theta1, y=theta2, fill=val)) +
-  geom_point(aes(x=mp_opt$mu_e_vec[1], y=mp_opt$mu_e_vec[2], color="posterior mean"), size=2) +
-  xlab("mu[1]") + ylab("mu[2]") +
-  scale_fill_gradient2()
-
-
-
-###############################
-# Plot the mu prior influence function
-
-# You could also do this more numerically stably with a Cholesky decomposition.
-lrvb_pre_factor <- -1 * lrvb_terms$jac %*% solve(lrvb_terms$elbo_hess)
-
-mu <- mp_opt$mu_e_vec + c(0.1, 0.2)
-GetInfluenceFunctionVector <- function(mu) {
-  mu_prior_val <- GetMuLogPrior(mu, pp)
-  mu_q_res <- GetMuLogDensity(mu, vp_opt, pp, TRUE)
-  exp(mu_q_res$val - mu_prior_val) * lrvb_pre_factor %*% mu_q_res$grad
-}
 
 # component <- mp_indices$mu_e_vec[1]; component_name <- "E_q[mu[1]]"
 # component <- mp_indices$mu_e_vec[2]; component_name <- "E_q[mu[2]]"
@@ -274,54 +234,30 @@ GetInfluenceFunctionVector <- function(mu) {
 # component <- mp_indices$tau[[1]]$e_log; component_name <- "E_q[log(tau[1])]"
 component <- mp_indices$mu_g[[7]]$e_vec[1]; component_name <- "E_q[mu_g[7]][1]"
 
-GetInfluenceFunctionComponent <- function(mu) GetInfluenceFunctionVector(mu)[component]
+comp_sens_vec <- unlist(lapply(sens_vec_list, function(entry) { as.numeric(entry[component]) } ))
+comp_influence_vec <- unlist(lapply(influence_vec_list, function(entry) { as.numeric(entry[component]) } ))
 
-width <- 2
-mu_influence <- EvaluateOn2dGrid(GetInfluenceFunctionComponent,
-                                 mp_opt$mu_e_vec, -width, width, -width, width, len=30)
-ggplot(mu_influence) +
-  geom_tile(aes(x=theta1, y=theta2, fill=val)) +
-  geom_point(aes(x=mp_opt$mu_e_vec[1], y=mp_opt$mu_e_vec[2], color="posterior mean"), size=2) +
-  scale_fill_gradient2() +
-  xlab("mu[1]") + ylab("mu[2]") +
-  ggtitle(paste("Influence of mu prior on ", component_name,
-                "\nCentered on the posterior", sep=""))
+component_df <- cbind(data.frame(sens_vec=comp_sens_vec, influence=comp_influence_vec), diagnostic_df)
 
-width <- 2
-q_mu <- EvaluateOn2dGrid(function(mu) { exp(GetMuLogDensity(mu, vp_opt, pp, FALSE)$val) },
-                         mp_opt$mu_e_vec, -width, width, -width, width, len=30)
-ggplot(q_mu) +
-  geom_tile(aes(x=theta1, y=theta2, fill=val)) +
-  geom_point(aes(x=mp_opt$mu_e_vec[1], y=mp_opt$mu_e_vec[2], color="posterior mean"), size=2) +
-  scale_fill_gradient2() +
-  xlab("mu[1]") + ylab("mu[2]") +
-  ggtitle(paste("Influence of mu prior on ", component_name,
-                "\nCentered on the posterior", sep=""))
+p1 <- ggplot(component_df) + geom_point(aes(x=X1, y=X2, color=sens_vec)) + scale_color_gradient2()
+p2 <- ggplot(component_df) + geom_point(aes(x=X1, y=X2, color=influence)) + scale_color_gradient2()
+p3 <- ggplot(component_df) + geom_point(aes(x=X1, y=X2, color=log10(prior_ratios))) + scale_color_gradient2()
 
+mean(component_df$influence)
 
-width <- 5
-mu_influence <- EvaluateOn2dGrid(GetInfluenceFunctionComponent,
-                                 pp$mu_loc, -width, width, -width, width, len=40)
-ggplot(mu_influence) +
-  geom_tile(aes(x=theta1, y=theta2, fill=val)) +
-  geom_point(aes(x=pp$mu_loc[1], y=pp$mu_loc[2], color="prior mean"), size=2) +
-  xlab("mu[1]") + ylab("mu[2]") +
-  scale_fill_gradient2()  +
-  ggtitle(paste("Influence of mu prior on ", component_name,
-                "\nCentered on the prior", sep=""))
+multiplot(p1, p2, p3)
 
+# ggplot(component_df) + geom_histogram(aes(x=log10(sens_vec)), bins=1000)
 
+mean(component_df$sens_vec)
+sens_vec_sd[component]
 
-
-################
-# Plot
-
-
-mean_results <- dcast(results, par + component + group ~ method, value.var="val")
-
-ggplot(mean_results) +
-  geom_point(aes(x=mfvb_norm, y=mfvb_t, color=par), size=3) +
-  geom_abline(aes(slope=1, intercept=0))
+# Estimate error
+u_dist <- u_draws - rep(colMeans(u_draws), each=nrow(u_draws))
+u_dist <- sqrt(rowSums(u_dist^2))
+u_extreme <- which(u_dist > quantile(u_dist, 0.95))
+max(abs(comp_influence_vec[u_extreme]))
+# ggplot(component_df[u_extreme, ]) + geom_point(aes(x=X1, y=X2, color=abs(influence))) + scale_color_gradient2()
 
 
 
