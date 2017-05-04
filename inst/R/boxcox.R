2ba91a08f703223e1262d7984bedbf3e765792e4
Ryan Giordano <rgiordan@gmail.com>
2017-05-04 16:49:41 -0700

    Make notebook

--- inst/R/boxcox.R
+++ inst/R/boxcox.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 library(Matrix)
+library(gridExtra)
 
 project_directory <-
   file.path(Sys.getenv("GIT_REPO_LOC"), "MicrocreditLRVB/inst/simulated_data")
@@ -28,18 +29,69 @@ x <- cbind(rep(1, length(y)), as.numeric(csv_data$treatment))
 
 data_df <- data.frame(x=x, y=y, y_g=y_g)
 
-# Group 3 has no negative profit?
-group <- 1
-ggplot(filter(data_df, y_g==group, abs(y) > 1e-8)) +
-  geom_histogram(aes(x=log(abs(y)), y=..density..), bins=100) +
-  facet_grid(y > 0 ~ .) + ggtitle(group)
+data_df_transform <-
+  data_df %>%
+  mutate(zero_y=abs(y) < 1e-8)
 
-group <- 2
-ggplot(filter(data_df, y_g==group, abs(y) > 1e-8, abs(y) < 500)) +
-  geom_histogram(aes(x=abs(y), y=..density..), bins=100) +
-  facet_grid(y > 0 ~ .) + ggtitle(group)
+#######################
+# Look at the raw distributions with outlier trimming.
 
-#####################
+trim_level <- 0.01
+y_quantiles <-
+  filter(data_df_transform, !zero_y) %>%
+  group_by(y_g) %>%
+  summarize(qlower=quantile(y, trim_level), qupper=quantile(y, 1 - trim_level))
+
+data_df_trim <-
+  inner_join(data_df_transform, y_quantiles, by="y_g") %>%
+  filter(y < qupper & y > qlower)
+
+ggplot(filter(data_df_trim, !zero_y)) +
+  geom_histogram(aes(x=y, y=..ndensity..), bins=50) +
+  facet_grid(~ y_g, scales="free") + 
+  geom_vline(aes(xintercept=0))
+
+
+
+###################
+# Look at cumulative distributions to check for power law behavior.
+# If it is a power law, the cumulative distribution will be a straight line
+# with slope given by the power law coefficient plus one.
+
+quantile_list <- list()
+for (group in 1:max(data_df_transform$y_g)) { for (y_sign in c(-1, 1)) { for (arm in c(0, 1)) {
+  rows <- with(data_df_transform, (y_g == group) & (!zero_y) & (y * y_sign > 0) & (x.2 == arm))
+  if (sum(rows) > 0) {
+    quantile_df <- data.frame(y=sort(y_sign * data_df_transform[rows, ]$y),
+                              quantile=(sum(rows):1) / sum(rows),
+                              group=group, y_sign=y_sign, arm=arm)
+    quantile_list[[length(quantile_list) + 1]] <- quantile_df
+  }
+}}}
+quantile_df <- do.call(rbind, quantile_list)
+
+grid.arrange(
+  ggplot(quantile_df) +
+    geom_point(aes(x=y_sign * log10(y), y=log10(quantile), color=factor(arm))) +
+    facet_grid(group ~ y_sign) +
+    ggtitle("Overlaid arms")
+,
+  ggplot(quantile_df) +
+    geom_point(aes(x=log10(y), y=log10(quantile), color=factor(y_sign))) +
+    facet_grid(group ~ arm) +
+    ggtitle("Overlaid signs")
+,
+  ggplot(quantile_df) +
+    geom_point(aes(x=log10(y), y=log10(quantile), color=paste(arm, y_sign))) +
+    facet_grid(group ~ .) +
+    ggtitle("Everything overlaid")
+, ncol=3
+)
+
+
+##########################################
+# Execute box-cox transforms and save.
+
 # A good reference for the Box-Cox transform:
 # https://www.ime.usp.br/~abe/lista/pdfm9cJKUmFZp.pdf
 library(MASS)
@@ -79,6 +131,7 @@ ggplot(filter(data_df_transform, !zero_y)) +
   geom_histogram(aes(x=y_trans, y=..density..), bins=100) +
   facet_grid(y_g ~ .)
 
+# Look at the lambdas chosen.  They are similar mostly except for group 2.
 mutate(data_df_transform, y_pos=y > 0) %>%
   filter(!zero_y) %>%
   group_by(y_g, y_pos) %>%
